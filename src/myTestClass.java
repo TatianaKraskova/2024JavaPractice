10d5bdb004849e6fe04176200963a06efb741044
Tatiana Kraskova <kraskova.qa@gmail.com>
2024-10-03 17:35:16 -0400

    first test

---
+++ src/myTestClass.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class myTestClass {
+
+    @Test
+    public void testAddition() {
+        int result = 2+4;
+        assertEquals(6, result);
+    }
+}
